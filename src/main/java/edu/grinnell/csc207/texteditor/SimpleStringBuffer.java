303236373c16ec0d35b3656070f434972ed7d151
Z Schwab <schwabz@grinnell.edu>
2025-09-23 12:00:29 -0500

    unit tests for simpleStringBuffer

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -4,6 +4,14 @@ package edu.grinnell.csc207.texteditor;
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
+    private String buffer;
+    private int cursor;
+
+    public SimpleStringBuffer() {
+        buffer = "";
+        cursor = 0;
+    }
+    
     public void insert(char ch) {
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
--- src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
@@ -1,5 +1,64 @@
 package edu.grinnell.csc207.texteditor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class SimpleStringBufferTests {
-    /** TODO: fill me in with unit and property tests! */
-}
+    
+    @Test
+    public void insertAtBackTest() {
+        SimpleStringBuffer testbuffer = new SimpleStringBuffer();
+        for (int i = 0; i < 20; i++) {
+            testbuffer.insert((char) (97+i));
+        }
+
+        assertEquals("abcdefghijklmnopqrs", testbuffer.toString());
+    }
+
+    @Test
+    public void deleteCharTest() {
+        SimpleStringBuffer testbuffer = new SimpleStringBuffer();
+        for (int i = 0; i < 20; i++) {
+            testbuffer.insert((char) (97+i));
+        }
+        testbuffer.delete();
+
+        assertEquals("abcdefghijklmnopqr", testbuffer.toString());
+    }
+
+    @Test
+    public void tryDeleteEmptyBufferTest() {
+        SimpleStringBuffer testbuffer = new SimpleStringBuffer();
+        testbuffer.delete();
+
+        assertEquals(0, testbuffer.getSize());
+        }
+
+    @Test
+    public void moveCursorTest() {
+        SimpleStringBuffer testbuffer = new SimpleStringBuffer();
+        for (int i = 0; i < 20; i++) {
+            testbuffer.insert((char) (97+i));
+        }
+        testbuffer.moveLeft();
+        testbuffer.moveLeft();
+        testbuffer.moveRight();
+
+        assertEquals(18, testbuffer.getCursorPosition());
+    }
+
+    @Test
+    public void tryPassBufferEndTest() {
+        SimpleStringBuffer testbuffer = new SimpleStringBuffer();
+        for (int i = 0; i < 20; i++) {
+            testbuffer.insert((char) (97+i));
+        }
+        testbuffer.moveRight();
+
+        assertEquals(19, testbuffer.getCursorPosition());
+    }
+
+    // add property tests here
+    
+ }
